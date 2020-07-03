183312cff574c5bf132bc2de6dd40de6f4d343b3
Miguel Sicart <deterbold@gmail.com>
2020-07-03 13:14:55 +0200

    Initial Commit

---
+++ Ridiculous Step Counter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ridiculous Step Counter
+//
+//  Created by Miguel Sicart on 03/07/2020.
+//  Copyright © 2020 Miguel Sicart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
